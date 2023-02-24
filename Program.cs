7a428dfcbe785c30d96ce2fd78edc38c07cd6972
Will Hedges <wshedges@outlook.com>
2023-02-24 14:45:57 -0600

    phase 1 complete * displays a message to the user asking them to guess the secret number * displays a prompt for the user's guess * takes the users guess and saves it to a variable * displays the user's guess back to the screen

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace GuessingGame
+Main();
+
+void Main()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    DisplayPrompt();
+}
+
+void DisplayPrompt()
+{
+    Console.WriteLine("Try to guess the secret number!");
+    Console.Write($"Enter your guess: ");
+    string guess = Console.ReadLine().Trim();
+    Console.Write($"You guessed {guess}.");
 }
