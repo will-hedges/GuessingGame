00103352fdac3c86224958cb0e9d21d5b644a824
Will Hedges <wshedges@outlook.com>
2023-02-24 15:14:50 -0600

    phase 3 complete * allows the user 4 chances to guess

--- Program.cs
+++ Program.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 
-Main();
+GuessingGame();
 
-void Main()
+void GuessingGame()
 {
-    int secret = SecretNumber(42);
-    GuessingGame(secret);
-}
+    int secretNumber = 42;
 
-void GuessingGame(int secretNumber)
-{
     Console.WriteLine("Try to guess the secret number!");
-    Console.Write($"Enter your guess: ");
 
-    int guess = Int32.Parse(Console.ReadLine().Trim());
-    if (guess == secretNumber)
+    for (int i = 0; i < 4; i++)
     {
-        Console.WriteLine($"You guessed correctly! The secret number was {secretNumber}.");
-    }
-    else
-    {
-        Console.WriteLine($"Sorry! {guess} was not the secret number.");
-    }
-}
+        Console.Write($"Enter your guess: ");
+        int guess = Int32.Parse(Console.ReadLine().Trim());
 
-int SecretNumber(int num)
-{
-    return num;
+        if (guess == secretNumber)
+        {
+            Console.WriteLine($"You guessed correctly! The secret number was {secretNumber}.");
+            return;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry! {guess} was not the secret number.");
+        }
+    }
+    Console.WriteLine("Better luck next time!");
 }
